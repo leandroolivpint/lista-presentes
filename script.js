028b54ee2bcdbfa12ca7911fcebded129fe104c0
Leandro de Oliveira Pinto <leandro.pinto@syngenta.com>
2026-07-30 11:40:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const lista = document.getElementById("lista");
 const sheetCSVUrl = "https://docs.google.com/spreadsheets/d/1uT-vwbjaJS2iP_H3J_gBFvkObGwucRMOx1B7qZaRXT4/export?format=csv&gid=0";
 const pixKey = "14841499636";
-const pixPayload = `00020126580014BR.GOV.BCB.PIX0114${pixKey}5204000053039865406450.005802BR5910SEU NOME6009SAO PAULO62070503***6304ABCD`;
 
 function parseCSV(text) {
   const rows = [];
@@ -114,10 +113,32 @@ function parseAmount(value) {
   return Number.isNaN(amount) ? 0 : amount;
 }
 
+function computeCrc16(text) {
+  let crc = 0xFFFF;
+  for (let i = 0; i < text.length; i++) {
+    crc ^= text.charCodeAt(i) << 8;
+    for (let j = 0; j < 8; j++) {
+      crc = (crc << 1) ^ (crc & 0x8000 ? 0x1021 : 0);
+      crc &= 0xFFFF;
+    }
+  }
+  return crc.toString(16).toUpperCase().padStart(4, "0");
+}
+
+function buildTag(id, value) {
+  return `${id}${String(value.length).padStart(2, "0")}${value}`;
+}
+
 function createPixPayload(amount) {
   const formatted = parseAmount(amount).toFixed(2);
-  const amountTag = `54${String(formatted.length).padStart(2, "0")}${formatted}`;
-  return `00020126580014BR.GOV.BCB.PIX0114${pixKey}5204000053039865406450.005802BR5910SEU NOME6009SAO PAULO${amountTag}6304ABCD`;
+  const amountTag = buildTag("54", formatted);
+  const merchantAccountInfo = buildTag("00", "BR.GOV.BCB.PIX") + buildTag("01", pixKey);
+  const merchantAccountInfoTag = buildTag("26", merchantAccountInfo);
+  const merchantName = buildTag("59", "SEU NOME");
+  const merchantCity = buildTag("60", "SAO PAULO");
+  const payloadWithoutCrc = `000201${merchantAccountInfoTag}520400005303986${amountTag}5802BR${merchantName}${merchantCity}6304`;
+  const crc = computeCrc16(payloadWithoutCrc);
+  return `${payloadWithoutCrc}${crc}`;
 }
 
 function createCard(item) {
